f750f9405f7aa754c4a11c0fad4a49caa2d786b9
David Nachtigall <peacekeeper@MBP-Retina-5.local>
2014-12-30 20:45:44 +0100

    Added functionality

---
+++ 99Balloons/Balloon.swift
@@ -0,0 +1,16 @@
+//
+//  Balloon.swift
+//  99Balloons
+//
+//  Created by David Nachtigall on 30.12.14.
+//  Copyright (c) 2014 David Nachtigall. All rights reserved.
+//
+
+import Foundation
+import UIkit
+
+struct Balloon {
+    var number = 0
+    var image = UIImage(named: "BerlinTVTower.jpg")
+    
+}
--- 99Balloons/ViewController.swift
+++ 99Balloons/ViewController.swift
@@ -9,10 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var myTextLabel: UILabel!
+    
 
+    var balloonsArray:[Balloon] = []
+    var ballonpicturenamesArray:[String] = []
+    var currentIndex = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myImageView.image = UIImage(named: "BerlinTVTower.jpg")
+        myTextLabel.textColor = UIColor(white: 0, alpha: 100)
+        myTextLabel.text = "Welcome. Press Next to display balloon pictures."
+        
+        // Make an Array with the names of the 4 BallonPicture Names for easier randomly picking them.
+        for var balloons = 1; balloons <= 4; balloons++ {
+            ballonpicturenamesArray.append("RedBalloon\(balloons).jpg")
+        }
+        
+        
+        // Call make Balloon 99 times and append each returned Balloon to the Balloons Array
+        for var i=1; i <= 99; i++ {
+            var balloon = makeBalloon()
+            balloon.number = i
+            balloonsArray.append(balloon)
+        }
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +47,46 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func nextButtonPressed(sender: AnyObject) {
+        // initalize random Index
+        var randomIndex:Int
+       
+        // Creat random index until it's not equal to the current index
+        
+        do {
+        randomIndex = Int(arc4random_uniform(UInt32(balloonsArray.count)))
+        } while currentIndex == randomIndex
+        
+        //set current index to the now accepted random index
+        
+        currentIndex = randomIndex
+        
+        //get the new current balloon from the array
+        
+        let currentBallon = balloonsArray[currentIndex]
+        
+        // Update Image View and Label with the properties of the fetched Balloon
+        
+        self.myImageView.image = currentBallon.image
+        self.myTextLabel.text = "Ballon No. \(currentBallon.number)"
+        
+    }
 
+    func makeBalloon() -> Balloon {
+        
+            // Generate Random Number to assign random Image
+            let randomNumber = Int(arc4random_uniform(UInt32(4)))
+        
+            //make the balloon from the struct, give it its random image and then return the Balloon
+            var madeBalloon = Balloon()
+            madeBalloon.image = UIImage(named: self.ballonpicturenamesArray[randomNumber])
+            return madeBalloon
+        
+        
+        
+        
+    }
+    
+    
 }
 
